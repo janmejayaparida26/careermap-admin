d039cd9740b29ab71684a345758994853f2b9f5c
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-04-23 11:08:59 +0530

    I have changed some code in Category2Table.jsx file

--- src/features/2ndcategories/Category2Table.jsx
+++ src/features/2ndcategories/Category2Table.jsx
@@ -34,7 +34,7 @@ export default function Category2Table({
         <img
           src={img}
           alt=""
-          className="w-12 h-12 rounded-md object-cover"
+          className="w-12 h-12 rounded-full object-cover"
         />
       ),
     },
