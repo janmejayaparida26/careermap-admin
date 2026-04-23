78efc4d2ec9ac495fc86e058e83432a3650b2ad7
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-04-23 15:46:40 +0530

    I have changed some code in SalaryPage.jsx file

--- src/features/salary/SalaryPage.jsx
+++ src/features/salary/SalaryPage.jsx
@@ -67,10 +67,10 @@ function SalaryPage() {
   };
 
   return (
-    <div className="p-6 w-full">
+    <div className="w-full">
       
       {/* Title */}
-      <h1 className="text-2xl font-semibold text-[#9a2119] mb-4">
+      <h1 className="text-xl font-semibold text-[#9a2119] mb-4">
         Salary Range Management
       </h1>
 
