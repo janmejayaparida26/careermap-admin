f1e1bde647826525ecfd62546d523c390f819af0
janmejayaparida26 <janmejayaparida789@gmail.com>
2026-04-27 12:30:05 +0530

    I have created CountriesForm.jsx file

---
+++ src/features/allcountries/CountriesForm.jsx
@@ -0,0 +1,27 @@
+import { Form, Input, Button } from "antd";
+
+export default function CountriesForm({ onSubmit, initialValues, viewMode }) {
+  const [form] = Form.useForm();
+
+  return (
+    <Form
+      layout="vertical"
+      form={form}
+      initialValues={initialValues}
+      onFinish={onSubmit}
+    >
+      <Form.Item name="name" label="Country Name">
+        <Input disabled={viewMode} />
+      </Form.Item>
+
+      {!viewMode && (
+        <Button
+          htmlType="submit"
+          className="bg-[#9a2119] text-white"
+        >
+          Submit
+        </Button>
+      )}
+    </Form>
+  );
+}
